ddb7ee86fd26645345223c701b43fb4bef5d8061
Mimi Ojserkis <mimioj@gmail.com>
2020-07-01 16:15:52 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -12,7 +12,11 @@ end
 introduction("Mimi")
 
 
+def introduction_with_language(name, language)
+  puts "Hi, My name is #{name} and I am learning to program in #{language}."
+end
 
+introduction_with_language("Mimi", "Ruby")
 
 
 
